979a18e6fc04448374f40301d20901b44229536b
Josh LeBlanc <joshcapitals07@gmail.com>
2025-05-08 13:49:13 +0200

    model understands the tool call. time to start for real

--- test.py
+++ test.py
@@ -8,35 +8,68 @@ def read_file(path):
 
     return content
 
-response = ollama.chat(
-    model='qwen3',
-    messages=[{'role': 'user', 'content':
-        'what is in the file main.py?'}],
+messages=[{'role': 'user', 'content':
+        'what is in the file main.py?'}]
 
-		# provide a weather checking tool to the model
-    tools=[{
-      'type': 'function',
-      'function': {
-        'name': 'read_file',
-        'description': "Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.",
-        'parameters': {
-          'type': 'object',
-          'properties': {
-            'path': {
-              'type': 'string',
-              'description': 'relative path to the file to be read',
+response: ollama.ChatResponse = ollama.chat(
+        model='qwen3',
+        messages=messages,
+        # provide a weather checking tool to the model
+        tools=[{
+            'type': 'function',
+            'function': {
+                'name': 'read_file',
+                'description': "Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.",
+                'parameters': {
+                    'type': 'object',
+                    'properties': {
+                        'path': {
+                            'type': 'string',
+                            'description': 'relative path to the file to be read',
+                            },
+                        },
+                    'required': ['path'],
+                    },
+                },
             },
-          },
-          'required': ['path'],
-        },
-      },
-    },
-  ],
-)
+               ],
+        )
 
-print(response['message']['tool_calls'])
+print(response.message.role)
 print("\n\nresponse:")
-print(response['message'])
-print("\n\ntool call result:")
-for tool_call in response['message']['tool_calls']:
-    print(globals()[tool_call.function.name](**tool_call.function.arguments))
+print(response.message)
+print("\n\nresponse with tool:")
+if response.message.tool_calls:
+    for tool_call in response.message.tool_calls:
+        tool_response = globals()[tool_call.function.name](**tool_call.function.arguments)
+        messages.append({
+            'role': 'tool',
+            'content': tool_response,
+            'name': tool_call.function.name
+            })
+
+response_with_tool: ollama.ChatResponse = ollama.chat(
+        model='qwen3',
+        messages=messages,
+        # provide a weather checking tool to the model
+        tools=[{
+            'type': 'function',
+            'function': {
+                'name': 'read_file',
+                'description': "Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.",
+                'parameters': {
+                    'type': 'object',
+                    'properties': {
+                        'path': {
+                            'type': 'string',
+                            'description': 'relative path to the file to be read',
+                            },
+                        },
+                    'required': ['path'],
+                    },
+                },
+            },
+               ],
+        )
+
+print(response_with_tool.message.content)
